16782d6316da47a80ca28cb598e9a16354140030
FloatZ1 <80592580+FloatZ1@users.noreply.github.com>
2026-01-27 14:21:37 +0200

    [rep

--- Engine/include/Scene/Octree.hpp
+++ Engine/include/Scene/Octree.hpp
@@ -1,9 +1,41 @@
 #ifndef EHAZ_OCTREE_IMPLEMENTATION_HPP
 #define EHAZ_OCTREE_IMPLEMENTATION_HPP
 
+#include "DataStructs.hpp"
+
+#include <cstdint>
+#include <vector>
+
+using namespace eHazGraphics;
+
 namespace eHaz {
 
-class COctree {};
+struct SOctNode {
+
+  uint32_t m_uiParentIndex;
+
+  AABB m_aabbLooseBounds;
+  AABB m_aabbTightBounds;
+
+  uint32_t m_ptrChildren[8]{0};
+
+  std::vector<uint32_t> m_vSceneObjectIds;
+
+  int m_iDepth = 0;
+};
+
+class COctree {
+
+public:
+  // returns a
+  std::vector<uint32_t> Query();
+
+  void Insert();
+
+private:
+  std::vector<SOctNode> m_onNodes;
+  std::vector<uint32_t> m_vFreeIndecies;
+};
 
 } // namespace eHaz
 
